1e35b8ebb5666d991bba2554342dba6413bbe7ee
xiangtao <xym_loveit@126.com>
2018-03-30 18:30:34 +0800

    add config server client

--- springCloud-configServerclient/src/main/java/com/xym/springcloud/controller/HelloController.java
+++ springCloud-configServerclient/src/main/java/com/xym/springcloud/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.xym.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xym
  */
+@RefreshScope
 @RestController
 public class HelloController {
 
