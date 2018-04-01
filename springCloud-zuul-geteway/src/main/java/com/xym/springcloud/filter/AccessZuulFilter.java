4502c5ccc6c45f7a90868e946bf96b8170f6dede
xiangtao <xym_loveit@126.com>
2018-04-01 23:56:14 +0800

    add zuul gateway

--- springCloud-zuul-geteway/src/main/java/com/xym/springcloud/filter/AccessZuulFilter.java
+++ springCloud-zuul-geteway/src/main/java/com/xym/springcloud/filter/AccessZuulFilter.java
@@ -8,6 +8,23 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletRequest;
 
 
+/**
+ * 在服务网关中定义过滤器只需要继承ZuulFilter抽象类实现其定义的四个抽象函数就可对请求进行拦截与过滤。
+ * <p>
+ * 自定义过滤器的实现，需要继承ZuulFilter，需要重写实现下面四个方法：
+ * <p>
+ * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+ * pre：可以在请求被路由之前调用
+ * routing：在路由请求时候被调用
+ * post：在routing和error过滤器之后被调用
+ * error：处理请求时发生错误时被调用
+ * <p>
+ * filterOrder：通过int值来定义过滤器的执行顺序
+ * shouldFilter：返回一个boolean类型来判断该过滤器是否要执行，所以通过此函数可实现过滤器的开关。在上例中，我们直接返回true，所以该过滤器总是生效。
+ * run：过滤器的具体逻辑。需要注意，这里我们通过ctx.setSendZuulResponse(false)令zuul过滤该请求，不对其进行路由，然后通过ctx.setResponseStatusCode(401)设置了其返回的错误码，当然我们也可以进一步优化我们的返回，比如，通过ctx.setResponseBody(body)对返回body内容进行编辑等。
+ *
+ * @author xym
+ */
 public class AccessZuulFilter extends ZuulFilter {
 
     private static Logger log = LoggerFactory.getLogger(AccessZuulFilter.class);
@@ -39,6 +56,7 @@ public class AccessZuulFilter extends ZuulFilter {
             log.warn("access token is empty");
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(401);
+            ctx.setResponseBody("<h3>Unauthorized Service!</h3>");
             return null;
         }
         log.info("access token ok");
