49134f74e8f03b53b46c7407e18e033ef4576d40
xiangtao <xym_loveit@126.com>
2018-04-10 00:16:40 +0800

    add zuul

---
+++ microservice-consumer-movie-feign/src/main/java/com/xym/springcloud/ConsumerMovieApplication.java
@@ -0,0 +1,27 @@
+package com.xym.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xym
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerMovieApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+
+}
