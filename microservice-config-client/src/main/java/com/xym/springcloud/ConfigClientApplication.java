01bec6c3b2d3b4b2701e366c5302eb7db7f097db
xiangtao <xym_loveit@126.com>
2018-04-11 12:36:01 +0800

    add zuul

---
+++ microservice-config-client/src/main/java/com/xym/springcloud/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.xym.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xym
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
